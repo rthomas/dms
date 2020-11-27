f4fb2c005b79099f87021d293fa0190d746929d5
Ryan Thomas <ryan@ryant.org>
2020-11-28 09:56:09 +1100

    next

--- src/message.rs
+++ src/message.rs
@@ -3,6 +3,7 @@ use nom::bytes::complete::take as take_bytes;
 use nom::combinator::map_res;
 use nom::IResult;
 use std::error::Error;
+use std::fmt;
 
 #[derive(Debug)]
 pub struct Message {
@@ -38,18 +39,87 @@ pub struct Flags {
 
 #[derive(Debug)]
 pub struct Question {
-    qnames: Vec<QName>,
-    qtype: QType,
+    qname: String,
+    qtype: Type,
     qclass: Class,
 }
 
 #[derive(Debug)]
 pub struct ResourceRecord {
-    name: Vec<QName>,
-    rtype: u16,
-    class: u16,
+    name: String,
+    rtype: Type,
+    class: Class,
     ttl: u32,
     rdata: Vec<u8>,
+}
+
+#[derive(Debug)]
+struct InnerQuestion {
+    qname: Vec<Name>,
+    qtype: Type,
+    qclass: Class,
+}
+
+fn flatten_to_string(names: &Vec<Name>) -> String {
+    let mut name = String::new();
+    for n in names.iter() {
+        match n {
+            Name::Name(part) => {
+                name.push_str(part);
+                name.push('.');
+            }
+            Name::ResolvedPtr(names) => {
+                let s = flatten_to_string(names);
+                name.push_str(&s);
+            }
+            Name::Pointer(i) => {
+                eprintln!("WARNING - FOUND UNRESOLVED POINTER....SKIPPING");
+            }
+        }
+    }
+    // Remove the trailing '.'
+    if name.chars().last() == Some('.') {
+        name.pop();
+    }
+    name
+}
+
+#[derive(Debug)]
+struct InnerResourceRecord {
+    name: Vec<Name>,
+    rtype: Type,
+    class: Class,
+    ttl: u32,
+    rdata: Vec<u8>,
+}
+
+impl From<InnerQuestion> for Question {
+    fn from(iq: InnerQuestion) -> Self {
+        Question {
+            qname: flatten_to_string(&iq.qname),
+            qtype: iq.qtype,
+            qclass: iq.qclass,
+        }
+    }
+}
+
+impl From<InnerResourceRecord> for ResourceRecord {
+    fn from(irr: InnerResourceRecord) -> Self {
+        ResourceRecord {
+            name: flatten_to_string(&irr.name),
+            rtype: irr.rtype,
+            class: irr.class,
+            ttl: irr.ttl,
+            rdata: irr.rdata,
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+enum Name {
+    Name(String),
+    Pointer(u16),
+    ResolvedPtr(Vec<Name>),
 }
 
 #[derive(Debug)]
@@ -62,55 +132,94 @@ pub enum OpCode {
 
 #[derive(Debug)]
 pub enum RCode {
-    NoError = 0,
-    FormatError = 1,
-    ServerFailure = 2,
-    NameError = 3,
-    NotImplemented = 4,
-    Refused = 5,
-    Unknown,
+    NoError,
+    FormatError,
+    ServerFailure,
+    NameError,
+    NotImplemented,
+    Refused,
+    Unknown(u8),
 }
 
 #[derive(Debug)]
-pub enum QType {
-    A = 1,
-    NS = 2,
-    MD = 3,
-    MF = 4,
-    CNAME = 5,
-    SOA = 6,
-    MB = 7,
-    MG = 8,
-    MR = 9,
-    NULL = 10,
-    WKS = 11,
-    PTR = 12,
-    HINFO = 13,
-    MINFO = 14,
-    MX = 15,
-    TXT = 16,
-    AXFR = 252,
-    MAILB = 253,
-    MAILA = 254,
-    STAR = 255,
-    Unknown,
+pub enum Type {
+    A,
+    NS,
+    MD,
+    MF,
+    CNAME,
+    SOA,
+    MB,
+    MG,
+    MR,
+    NULL,
+    WKS,
+    PTR,
+    HINFO,
+    MINFO,
+    MX,
+    TXT,
+    AXFR,
+    MAILB,
+    MAILA,
+    STAR,
+    Unknown(u16),
+}
+
+impl From<u16> for Type {
+    fn from(val: u16) -> Self {
+        match val {
+            1 => Type::A,
+            2 => Type::NS,
+            3 => Type::MD,
+            4 => Type::MF,
+            5 => Type::CNAME,
+            6 => Type::SOA,
+            7 => Type::MB,
+            8 => Type::MG,
+            9 => Type::MR,
+            10 => Type::NULL,
+            11 => Type::WKS,
+            12 => Type::PTR,
+            13 => Type::HINFO,
+            14 => Type::MINFO,
+            15 => Type::MX,
+            16 => Type::TXT,
+            252 => Type::AXFR,
+            253 => Type::MAILB,
+            254 => Type::MAILA,
+            255 => Type::STAR,
+            _ => Type::Unknown(val),
+        }
+    }
 }
 
 #[derive(Debug)]
 pub enum Class {
-    IN = 1,
-    CS = 2,
-    CH = 3,
-    HS = 4,
-    STAR = 255,
-    Unknown,
+    IN,
+    CS,
+    CH,
+    HS,
+    STAR,
+    Unknown(u16),
+}
+
+impl From<u16> for Class {
+    fn from(val: u16) -> Self {
+        match val {
+            1 => Class::IN,
+            2 => Class::CS,
+            3 => Class::CH,
+            4 => Class::HS,
+            255 => Class::STAR,
+            _ => Class::Unknown(val),
+        }
+    }
 }
 
 impl Message {
     pub fn parse<'a>(input: &'a [u8]) -> Result<Message, Box<dyn Error + 'a>> {
         let (_, message) = read_message(input)?;
-
-        // TODO - we now need to resolve all of the QName::Pointer records
 
         Ok(message)
     }
@@ -164,7 +273,7 @@ fn as_flags<'a>(input: &'a [u8]) -> Result<Flags, Box<dyn Error + 'a>> {
                 (i, 3) => (i, RCode::NameError),
                 (i, 4) => (i, RCode::NotImplemented),
                 (i, 5) => (i, RCode::Refused),
-                (i, _) => (i, RCode::Unknown),
+                (i, x) => (i, RCode::Unknown(x)),
             };
             Ok(((i), (qr, opcode, aa, tc, rd, ra, ad, cd, rcode)))
         })(input)?;
@@ -208,23 +317,17 @@ fn read_header(input: &[u8]) -> IResult<&[u8], Header> {
     map_res(take_bytes(12usize), as_header)(input)
 }
 
-fn as_qname(input: &[u8]) -> Result<QName, Box<dyn Error>> {
-    Ok(QName::Name(std::str::from_utf8(input)?.to_string()))
+fn as_name(input: &[u8]) -> Result<Name, Box<dyn Error>> {
+    Ok(Name::Name(std::str::from_utf8(input)?.to_string()))
 }
 
-#[derive(Debug)]
-enum QName {
-    Name(String),
-    Pointer(u16),
-}
-
-fn read_qnames(input: &[u8]) -> IResult<&[u8], Vec<QName>> {
+fn read_names(input: &[u8]) -> IResult<&[u8], Vec<Name>> {
     use nom::bits::bits;
 
-    let mut qnames = Vec::new();
+    let mut qname = Vec::new();
     let mut input = input;
 
-    /// Helper for pulling out either the length of a QName segment, or the
+    /// Helper for pulling out either the length of a Name segment, or the
     /// offset of the name to parse.
     enum NameRecord {
         Offset(u16),
@@ -249,7 +352,7 @@ fn read_qnames(input: &[u8]) -> IResult<&[u8], Vec<QName>> {
 
         match name_record {
             NameRecord::Offset(offset) => {
-                qnames.push(QName::Pointer(offset));
+                qname.push(Name::Pointer(offset));
                 input = i;
                 break;
             }
@@ -260,69 +363,54 @@ fn read_qnames(input: &[u8]) -> IResult<&[u8], Vec<QName>> {
                     break;
                 }
 
-                let (i, qname) = map_res(take_bytes(length), as_qname)(i)?;
-                qnames.push(qname);
+                let (i, name) = map_res(take_bytes(length), as_name)(i)?;
+                qname.push(name);
                 input = i;
             }
         }
     }
-    Ok((input, qnames))
+    Ok((input, qname))
 }
 
-fn read_question(input: &[u8]) -> IResult<&[u8], Question> {
-    let (input, qnames) = read_qnames(input)?;
+fn read_question(input: &[u8]) -> IResult<&[u8], InnerQuestion> {
+    let (input, qname) = read_names(input)?;
 
-    let (input, qtype) = match read_u16(input)? {
-        (i, 1) => (i, QType::A),
-        (i, 2) => (i, QType::NS),
-        (i, 3) => (i, QType::MD),
-        (i, 4) => (i, QType::MF),
-        (i, 5) => (i, QType::CNAME),
-        (i, 6) => (i, QType::SOA),
-        (i, 7) => (i, QType::MB),
-        (i, 8) => (i, QType::MG),
-        (i, 9) => (i, QType::MR),
-        (i, 10) => (i, QType::NULL),
-        (i, 11) => (i, QType::WKS),
-        (i, 12) => (i, QType::PTR),
-        (i, 13) => (i, QType::HINFO),
-        (i, 14) => (i, QType::MINFO),
-        (i, 15) => (i, QType::MX),
-        (i, 16) => (i, QType::TXT),
-        (i, 252) => (i, QType::AXFR),
-        (i, 253) => (i, QType::MAILB),
-        (i, 254) => (i, QType::MAILA),
-        (i, 255) => (i, QType::STAR),
-        (i, _) => (i, QType::Unknown),
+    let (input, qtype) = {
+        let (i, q) = read_u16(input)?;
+        (i, Type::from(q))
     };
-    let (input, qclass) = match read_u16(input)? {
-        (i, 1) => (i, Class::IN),
-        (i, 2) => (i, Class::CS),
-        (i, 3) => (i, Class::CH),
-        (i, 4) => (i, Class::HS),
-        (i, 255) => (i, Class::STAR),
-        (i, _) => (i, Class::Unknown),
+    let (input, qclass) = {
+        let (i, c) = read_u16(input)?;
+        (i, Class::from(c))
     };
 
-    let question = Question {
-        qnames,
-        qtype,
-        qclass,
-    };
-    Ok((input, question))
+    Ok((
+        input,
+        InnerQuestion {
+            qname,
+            qtype,
+            qclass,
+        },
+    ))
 }
 
-fn read_resource_record(input: &[u8]) -> IResult<&[u8], ResourceRecord> {
-    let (input, name) = read_qnames(input)?;
-    let (input, rtype) = read_u16(input)?;
-    let (input, class) = read_u16(input)?;
+fn read_resource_record(input: &[u8]) -> IResult<&[u8], InnerResourceRecord> {
+    let (input, name) = read_names(input)?;
+    let (input, rtype) = {
+        let (i, r) = read_u16(input)?;
+        (i, Type::from(r))
+    };
+    let (input, class) = {
+        let (i, c) = read_u16(input)?;
+        (i, Class::from(c))
+    };
     let (input, ttl) = read_u32(input)?;
     let (input, rdlength) = read_u16(input)?;
     let (_, rdata) = take_bytes(rdlength)(input)?;
     let rdata = Vec::from(rdata);
     Ok((
         input,
-        ResourceRecord {
+        InnerResourceRecord {
             name,
             rtype,
             class,
@@ -333,6 +421,7 @@ fn read_resource_record(input: &[u8]) -> IResult<&[u8], ResourceRecord> {
 }
 
 fn as_message<'a>(input: &'a [u8]) -> Result<Message, Box<dyn Error + 'a>> {
+    let original_input = input;
     let (mut input, header) = read_header(input)?;
 
     let mut questions = Vec::new();
@@ -363,13 +452,74 @@ fn as_message<'a>(input: &'a [u8]) -> Result<Message, Box<dyn Error + 'a>> {
         additional_records.push(record);
     }
 
+    // Resolve the Name::Pointer records.
+    for q in questions.iter_mut() {
+        resolve_names(original_input, &mut q.qname)?;
+    }
+    for a in answers.iter_mut() {
+        resolve_names(original_input, &mut a.name)?;
+    }
+    for a in name_servers.iter_mut() {
+        resolve_names(original_input, &mut a.name)?;
+    }
+    for a in additional_records.iter_mut() {
+        resolve_names(original_input, &mut a.name)?;
+    }
+
     Ok(Message {
         header,
-        questions,
-        answers,
-        name_servers,
-        additional_records,
+        questions: questions.drain(..).map(Question::from).collect(),
+        answers: answers.drain(..).map(ResourceRecord::from).collect(),
+        name_servers: name_servers.drain(..).map(ResourceRecord::from).collect(),
+        additional_records: additional_records
+            .drain(..)
+            .map(ResourceRecord::from)
+            .collect(),
     })
+}
+
+#[derive(Debug)]
+pub struct InvalidMessageError {
+    message: String,
+}
+
+impl fmt::Display for InvalidMessageError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+        write!(f, "InvalidMessageError: {}", self.message)?;
+        Ok(())
+    }
+}
+
+impl Error for InvalidMessageError {}
+
+/// This just does a single level of pointer resolution TODO - we should
+/// dereference all of the pointers, rather than a single level - and need
+/// to add loop protection even for the case of a single pointer pointing back to itself.
+fn resolve_names<'a>(input: &'a [u8], names: &mut Vec<Name>) -> Result<(), Box<dyn Error + 'a>> {
+    use std::collections::HashSet;
+
+    let mut seen_ptrs = HashSet::new();
+
+    for n in names.iter_mut() {
+        match n {
+            Name::Pointer(ptr) => {
+                if seen_ptrs.contains(ptr) {
+                    return Err(Box::new(InvalidMessageError {
+                        message: format!(
+                            "Circular reference - detected a pointer we have seen already: {}",
+                            *ptr
+                        )
+                        .to_string(),
+                    }));
+                }
+                seen_ptrs.insert(*ptr);
+                let (_, names) = read_names(&input[*ptr as usize..input.len()])?;
+                *n = Name::ResolvedPtr(names);
+            }
+            _ => {}
+        }
+    }
+    Ok(())
 }
 
 fn read_message(input: &[u8]) -> IResult<&[u8], Message> {
